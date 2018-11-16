fa88ff3cf1ec2486e6d843b108bcdc80ed94205a
M. Emre Aydin <emraydin@gmail.com>
2018-11-16 14:47:16 +0300

    export filters script

---
+++ scripts/vtzim_export_filters.py
@@ -0,0 +1,39 @@
+import subprocess
+import pickle
+import argparse
+import os
+
+
+def get_filters(user, verbose=False):
+    ps = subprocess.Popen(["zmprov", "ga", user, "zimbraMailSieveScript"], stdout=subprocess.PIPE)
+    ps2 = subprocess.Popen(["sed", "-e", "1d"], stdin=ps.stdout, stdout=subprocess.PIPE)
+    ps3 = subprocess.check_output(["sed", "s/zimbraMailSieveScript: //g"], stdin=ps2.stdout)
+    return ps3
+
+
+def save_file(filters, user, path, verbose=False):
+    file_path = os.path.join(path, user + ".pickle")
+    try:
+        with open(file_path, 'wb') as fp:
+            pickle.dump(filters, fp, protocol=pickle.HIGHEST_PROTOCOL)
+        return True
+    except Exception as err:
+        print("Error while dumping pickle: {0}".format(str(err)))
+        return False
+
+
+def main():
+    filters = get_filters(args.user, args.verbose)
+    save_file(filters, args.user, args.path, args.verbose)
+    print("Filter save success for user {user} to {path}".format(path=args.path, user=args.user))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog='export_filters.py', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--user', '-u', help='Username to export', required=True)
+    parser.add_argument('--path', '-p', help='Path to write to', default='.')
+    parser.add_argument('--verbose', '-v', help='Chatty guy', action='store_true')
+
+    args = parser.parse_args()
+
+    main()
