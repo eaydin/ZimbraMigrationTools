fd385cddb00ec39dd73f213f3d98f4e8f4e7ef1b
M. Emre Aydin <emraydin@gmail.com>
2018-11-16 14:56:36 +0300

    export signatures script

---
+++ scripts/export_signatures.py
@@ -0,0 +1,79 @@
+from subprocess import check_output
+import sys
+import pickle
+import argparse
+import os
+
+
+def main():
+    user = args.user
+
+    try:
+        sigs = check_output(["zmprov", "gsig", user])
+    except Exception as err:
+        print("Error while using zmprov. Probably you're not zimbra user? Error message: {0}".format(str(err)))
+        raise SystemExit
+
+    sigs_lines = sigs.split('\n')
+
+    signatures_html = {}
+    signatures_plain = {}
+
+    new_sig = True
+    html = False
+
+    for line in sigs_lines:
+        if new_sig:
+            if line.startswith("zimbraPrefMailSignature"):
+                new_sig = False
+                if "HTML" in line[:28]:
+                    html = True
+                    temp_sig = line[28:].strip()
+                else:
+                    html = False
+                    temp_sig = line[24:].strip()
+                continue
+        elif line.startswith("zimbraSignatureId:"):
+            continue
+        elif line.startswith("zimbraSignatureName:"):
+            temp_name = line[20:].strip()
+            new_sig = True
+            if html:
+                signatures_html[temp_name] = temp_sig
+            else:
+                signatures_plain[temp_name] = temp_sig
+        elif line.startswith("#"):
+            continue
+        else:
+            temp_sig += line.strip()
+
+    if args.verbose:
+        print(signatures)
+
+    pickle_file_path = os.path.join(args.path, user + ".pickle")
+
+    master_pickle = {}
+    master_pickle['HTML'] = signatures_html
+    master_pickle['PLAIN'] = signatures_plain
+
+    with open(pickle_file_path, 'wb') as fp:
+        pickle.dump(master_pickle, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+    return pickle_file_path
+
+if __name__ == '__main__':
+
+    if len(sys.argv) < 2:
+        raise SystemExit("Please Provide Username")
+
+    parser = argparse.ArgumentParser(prog='export_signatures.py', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--user', '-u', help='Username to export', required=True)
+    parser.add_argument('--path', '-p', help='Path to write to', default='.')
+    parser.add_argument('--verbose', '-v', help='Chatty guy', action='store_true')
+
+    args = parser.parse_args()
+
+    saved = main()
+
+    print("Completed for user: {0} path: {1}".format(args.user, saved))
