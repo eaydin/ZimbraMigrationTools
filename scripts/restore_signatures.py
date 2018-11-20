6f273d1d32ae48416f7fcd6e51a51a30fa53276a
M. Emre Aydin <emraydin@gmail.com>
2018-11-20 22:04:20 +0300

    exit status handles and reflects errors

--- scripts/restore_signatures.py
+++ scripts/restore_signatures.py
@@ -1,10 +1,12 @@
 from subprocess import check_output
 import argparse
 from VTZM import helpers
+import sys
 
 
 def main():
 
+    errors = False
     master_signatures = helpers.read_pickle(args.file)
     if master_signatures:
 
@@ -13,20 +15,31 @@ def main():
 
         for signame in signatures_html:
 
-            create_sigs = check_output(["zmprov", "csig", args.user, signame, "zimbraPrefMailSignatureHTML", signatures_html[signame]])
-
-            if args.verbose:
-                print(create_sigs)
+            try:
+                create_sigs = check_output(["zmprov", "csig", args.user, signame, "zimbraPrefMailSignatureHTML", signatures_html[signame]])
+            except Exception as err:
+                print("Error while running zmprov csig command for HTML Signature: {0}".format(str(err)))
+                errors = True
+            else:
+                if args.verbose:
+                    print(create_sigs)
 
         for signame in signatures_plain:
 
-            create_sigs = check_output(["zmprov", "csig", args.user, signame, "zimbraPrefMailSignature", signatures_plain[signame]])
-
-            if args.verbose:
-                print(create_sigs)
+            try:
+                create_sigs = check_output(["zmprov", "csig", args.user, signame, "zimbraPrefMailSignature", signatures_plain[signame]])
+            except Exception as err:
+                print("Error while running zmprov csig command for Plain Text Signature: {0}".format(str(err)))
+                errors = True
+            else:
+                if args.verbose:
+                    print(create_sigs)
 
     else:
         print("Error while reading signatures.")
+        sys.exit(1)
+
+    return errors
 
 
 if __name__ == '__main__':
@@ -39,6 +52,11 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    main()
+    status = main()
 
-    print("Completed for user: {0} pickle: {1}".format(args.user, args.file))
+    if status:
+        print("Signature restore for user {0} completed with errors!".format(args.user))
+        sys.exit(1)
+    else:
+        print("Signature restore completed for user: {0} pickle: {1}".format(args.user, args.file))
+        sys.exit(0)
