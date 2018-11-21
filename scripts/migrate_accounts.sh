8991dd25f1cbb8cd70f1bd81caeead921946b6ac
M. Emre Aydin <emraydin@gmail.com>
2018-11-21 16:29:57 +0300

    handling wildcard as an argument

--- scripts/migrate_accounts.sh
+++ scripts/migrate_accounts.sh
@@ -18,11 +18,11 @@ log_line "Migrating users.txt"
 send_file ${OLD_ACCOUNTS_PATH} users.txt ${NEW_ACCOUNTS_PATH}
 
 log_line "Migrating account details"
-send_file ${OLD_ACCOUNT_DETAILS_PATH} * ${NEW_ACCOUNT_DETAILS_PATH}
+send_file ${OLD_ACCOUNT_DETAILS_PATH} \* ${NEW_ACCOUNT_DETAILS_PATH}
 check_err "Error while migrating account details"
 
 log_line "Migrating account passwords"
-send_file ${OLD_PASSWORDS_PATH} * ${NEW_PASSWORDS_PATH}
+send_file ${OLD_PASSWORDS_PATH} \* ${NEW_PASSWORDS_PATH}
 check_err "Error while migrating account passwords"
 
 log_line "Migrating distribution lists"
@@ -30,11 +30,11 @@ send_file ${OLD_DIST_LISTS_PATH} dist_lists.txt ${NEW_DIST_LISTS_PATH}
 check_err "Error while migrating dist_lists.txt"
 
 log_line "Migrating alises"
-send_file ${OLD_ALIASES_PATH} * ${NEW_ALIASES_PATH}
+send_file ${OLD_ALIASES_PATH} \* ${NEW_ALIASES_PATH}
 check_err "Error while migrating account aliases"
 
 log_line "Migrating Signatures"
-send_file ${OLD_SIGNATURES_PATH} * ${NEW_SIGNATURES_PATH}
+send_file ${OLD_SIGNATURES_PATH} \* ${NEW_SIGNATURES_PATH}
 check_err "Error while sending signatures"
 
 log_line "File based migrations complete"
